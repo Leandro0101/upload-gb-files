4a7c0bb68f5cfbcae6e35cd89ec83081e79d7375
Leandro0101 <leandrovieira3306@gmail.com>
2021-03-21 22:41:14 -0300

    add app

---
+++ frontend/public/app.js
@@ -0,0 +1,11 @@
+const showSize = () => {
+  const files = document.getElementById('files')
+  debugger
+}
+
+const onload = () => {
+  console.log('loaded')
+}
+
+window.onload = onload
+window.showSize = showSize
