994656ec3852c8417f6750c9bad1f16a899bb176
Leandro0101 <leandrovieira3306@gmail.com>
2021-03-24 18:02:04 -0300

    add socketio at backend

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const socketIo = require('socket.io')
+const PORT = 3000
+
+const handler = function (request, response) {
+  const defaultRoute = async (request, response) => response.end('Hello!')
+
+  return defaultRoute(request, response)
+}
+
+const server = http.createServer(handler)
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+    credentials: false
+  }
+})
+
+io.on('connection', socket => console.log('someone connected', socket.id))
+
+setInterval(() => {
+  io.emit('file-upload', 5e6)
+}, 250)
+
+const startServer = () => {
+  const { address, port } = server.address()
+  console.log(`app running at http://${address}:${port}`)
+}
+
+server.listen(PORT, startServer)
